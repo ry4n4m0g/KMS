3ef3bb14cd5174781c6f8f3a612d7837401aa9c8
Frank <fvaland@fintechadvance.com>
2018-05-22 17:45:06 -0700

    Fixed migrations (missing current updates).

--- app/migrations/00002_users.sql
+++ app/migrations/00002_users.sql
@@ -1,14 +1,15 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-CREATE TABLE IF NOT EXISTS `keys` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `key` varchar(80) NOT NULL,
+  `username` varchar(80) NOT NULL,
+  `password` varchar(80) NOT NULL,
+  `program_id` int(11) NOT NULL,
   `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`)
+  UNIQUE KEY `username` (`username`,`program_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-DROP TABLE IF EXISTS `keys`;
+DROP TABLE IF EXISTS `users`;
